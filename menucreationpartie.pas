e662c5a66115ab14511d6c9b8bd7eed22d3a4f8e
MathisPct <math712b@gmail.com>
2020-12-02 15:24:05 +0100

    meilleur mise en forme code

--- menucreationpartie.pas
+++ menucreationpartie.pas
@@ -50,12 +50,13 @@ const
     joueur : perso; //variable de type perso (record issu de l'unité personnage)
 
 
- {Procédure qui affiche tous les items du menu en position X et Y}
+{ Procédure qui affiche tous les items du menu en position X et Y   }
   procedure affichageItemsMenu();
   begin
     //affichage des items du menu  (affichageItem est une fonction de gestionEcran)
     affichageItem(menuCreaPartie[1],itemsCoordX[1],itemsCoordY[1]); //affichage de l'item 1 du menu avec les coordonnées de cette item
   end;
+
 
   {Procédure qui colorier l'élément actuel sur lequel est placé l'utilisateur}
   procedure colorierElementActuel();
@@ -94,36 +95,35 @@ const
 
   {Procédure qui appelle toutes les fonctions et procédures pour afficher et interragir avec le menu de création de partie }
   procedure mainMenuCreaPartie();
-  var
-     running: Boolean; //variable booleenne qui permet de demarrer le menu
-  begin
-     initiaNbTourBoucle(); //initialisation du nb de tour de boucle quand on arrive sur le menu
-     running:=True; //initialisation de running à true quand on arrive sur le menu
-     //tant que le menu est lancé executé les instructions
-     while (running=True) do
-           begin
-             //si on vient d'arriver sur le menu on initialise l'affichage des éléments du menu, initialisation de l'item actuel à 1 etc
-             if (getNbTourBoucle()=0) then
-             begin
-               effacerEcran; //raffraichissement de l'écran car on est passé sur un autre menu
-              //initialisation de l'item actuel au 1er item du menu quand on arrive sur le menu
-              initialisationItemActuel(1);
-              //initialisation de l'item antérieur à itemActuel-1 quand on arrive sur le menu
-              initialisationItemAnterieur();
-              //affichage des rectangles, du texte et du menu
-              affichage();// affichage des rectangles du nom du menu et de tous les items du menu
-             end
-             else if(getNbTourBoucle>=1) then
+     var
+        running: Boolean; //variable booleenne qui permet de demarrer le menu
+     begin
+        initiaNbTourBoucle(); //initialisation du nb de tour de boucle quand on arrive sur le menu
+        running:=True; //initialisation de running à true quand on arrive sur le menu
+        //tant que le menu est lancé executé les instructions
+        while (running=True) do
+          begin
+              //si on vient d'arriver sur le menu on initialise l'affichage des éléments du menu, initialisation de l'item actuel à 1 etc
+              if (getNbTourBoucle()=0) then
                 begin
-                  //touche:= GetKeyEvent; //GetKeyEvent est une fonction de l'unité Keyboard qui renvoit les évènements du clavier
-                  //touche:= TranslateKeyEvent(touche); //retourne la valeur unicode de la touche si elle est pressée . Variable de type int
+                  effacerEcran; //raffraichissement de l'écran car on est passé sur un autre menu
+                  //initialisation de l'item actuel au 1er item du menu quand on arrive sur le menu
+                  initialisationItemActuel(1);
+                  //initialisation de l'item antérieur à itemActuel-1 quand on arrive sur le menu
+                  initialisationItemAnterieur();
+                  //affichage des rectangles, du texte et du menu
+                  affichage();// affichage des rectangles du nom du menu et de tous les items du menu
+                end
+              else if(getNbTourBoucle>=1) then
+                begin
                   navigationTabMenu(menuCreaPartie,touche,getItemActuel());//appel de la procédure qui permet de naviguer dans le tableau du menu, tant qu'on a pas choisi une option dans le menu, on reste dans le menu
                   saisieNom(); //procédure de saisie du nom du personnage
                   running:=False; //fin du menu quand le nom a été saisi
                 end;
-             incrementaNbTourBoucle(); //incrémentation du tour de boucle
-           end;
-  end;
+              incrementaNbTourBoucle(); //incrémentation du tour de boucle
+          end;  //fin tant que
+
+       end;  //fin procédure
 
 end.
 
